722f4ae662c69af0ea95108fe237118713fefdde
mahadi5988 <102424117+mahadi5988@users.noreply.github.com>
2022-06-19 03:12:10 -0700

    Add files via upload

---
+++ lab2.php
@@ -0,0 +1,123 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = $dateErr = $degree = "" ;
+$name = $email = $gender = $comment = $website = $date = $degree = "" ;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = $_POST["name"];
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = $_POST["email"];
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = $_POST["website"];
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!filter_var($website, FILTER_VALIDATE_URL)) {
+      $websiteErr = "Invalid email format";
+      $website = "";
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = $_POST["comment"];
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = $_POST["gender"];
+  }
+  if (empty($_POST["date"])) {
+    $dateErr = "Date Of Birth is required";
+  } else {
+    $date = $_POST["date"];
+  }
+  if (empty($_POST["degree"])) {
+    $degreeErr = $_POST['degree'];
+   $count = count($degree);
+   echo "There are ".$count." checkboxe(s) are checked";
+  } else {
+    $degree = $_POST["degree"];
+  }
+  
+ }
+
+
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  Date:
+	<input type ="date" name = "date" value = "<?php echo date('d-m-y'); ?>" /><br><br>
+  Degree:<br>
+	<input type = "checkbox" value = "box" name = "SSC[]"/> 
+	<input type = "checkbox" value = "box" name = "SSC[]"/> 
+	<input type = "checkbox" value = "box" name = "SSC[]"/> <br><br>
+
+	
+	<input type="submit" name="submit" value="Submit"> 
+	
+	
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
